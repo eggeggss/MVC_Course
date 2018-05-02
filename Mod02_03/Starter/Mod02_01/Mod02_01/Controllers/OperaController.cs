57f6890c67d499884248a23972d50634c19c4837
eggeggss <eggeggss@gmail.com>
2018-05-02 20:50:16 +0800

    v3

---
+++ Mod02_03/Starter/Mod02_01/Mod02_01/Controllers/OperaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mod02_01.Models;
+namespace Mod02_01.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        public ActionResult Index(Opera opera)
+        {
+            Opera o = new Opera()
+            {
+                OperaID = opera.OperaID,
+                Title = opera.Title,
+                Year = opera.Year,
+                Composer = opera.Composer
+            };
+            return View(o);
+        }
+
+
+    }
+}
---
+++ Mod02_04/Starter/Mod02_01/Mod02_01/Controllers/OperaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mod02_01.Models;
+using Mod02_01.DAL;
+namespace Mod02_01.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        private OperaContext context = new OperaContext();
+        public ActionResult Index()
+        {
+
+            return View(context.Operas.ToList());
+        }
+
+
+
+    }
+}

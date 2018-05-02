3d3a84876fe370428accafd0915a781b6a42b23e
eggeggss <eggeggss@gmail.com>
2018-05-02 20:49:00 +0800

    v2

---
+++ Mod02_01/Starter/Mod02_01/Mod02_01/Controllers/OperaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mod02_01.Models;
+namespace Mod02_01.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        public ActionResult Index()
+        {
+            Opera o = new Opera()
+            {
+                OperaID = 1,
+                Title = "尤利迪西",
+                Year = 1600,
+                Composer = "佩里"
+            };
+            return View(o);
+
+        }
+    }
+}

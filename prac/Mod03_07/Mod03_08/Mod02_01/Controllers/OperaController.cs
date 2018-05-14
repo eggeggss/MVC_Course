8d5ef2a44ae6ea939b15fdf6cb84152baea2e468
eggeggss <eggeggss@gmail.com>
2018-05-14 18:39:04 +0800

    v1

---
+++ prac/Mod03_07/Mod03_08/Mod02_01/Controllers/OperaController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mod02_01.Models;
+using Mod02_01.DAL;
+using System.Net;
+using System.Data.Entity;
+
+namespace Mod02_01.Controllers
+{
+    public class OperaController : Controller
+    {
+
+        //建立DB物件
+        private OperaContext context = new OperaContext();
+
+        // GET: Opera
+        // GET: Opera/Index
+        public ActionResult Index()
+        {
+            return View(context.Operas.ToList());
+        }
+
+        // GET: Opera/Details/1
+        // GET: Opera/Details?id=1
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Opera o = context.Operas.Find(id);
+
+            if (o == null)
+                return HttpNotFound();
+
+            return View(o);
+        }
+
+        // GET: Opera/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Opera/Create
+        [HttpPost]
+        public ActionResult Create(Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Operas.Add(opera);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(opera);
+        }
+
+        // GET: Opera/Edit/1
+        // GET: Opera/Edit?id=1
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Opera o = context.Operas.Find(id);
+            if (o == null)
+            {
+                return HttpNotFound();
+            }
+            return View(o);
+        }
+
+        // POST: Opera/Edit
+        [HttpPost]
+        public ActionResult Edit(Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(opera).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(opera);
+        }
+
+        // GET: Opera/Delete/1
+        // GET: Opera/Delete?id=1
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+        //    Opera o = context.Operas.Find(id);
+        //    if (o == null)
+        //        return HttpNotFound();
+
+        //    return View(o);
+        //}
+
+        // POST: Opera/Delete/1
+        //[HttpPost, ActionName("Delete")]
+        //public ActionResult DeleteConfirmed(int? id)
+        //{
+        //    Opera o = context.Operas.Find(id);
+        //    context.Operas.Remove(o);
+        //    context.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        // GET: Opera/Delete/1
+        // GET: Opera/Delete?id=1
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Opera o = context.Operas.Find(id);
+            if (o == null)
+                return HttpNotFound();
+            context.Operas.Remove(o);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ prac/Mod03_07/Starter/Mod02_01/Mod02_01/Controllers/OperaController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mod02_01.Models;
+using Mod02_01.DAL;
+using System.Net;
+using System.Data.Entity;
+
+namespace Mod02_01.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        private OperaContext context = new OperaContext();
+        public ActionResult Index()
+        {
+
+            return View(context.Operas.ToList());
+        }
+
+
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new  HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Opera o = context.Operas.FirstOrDefault(m => m.OperaID == id);
+
+            if (o == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(o);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Operas.Add(opera);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(opera);
+            
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Opera o = context.Operas.FirstOrDefault(m => m.OperaID == id);
+
+            if (o == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(o);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(opera).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(opera);
+
+        }
+        /*
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Opera o = context.Operas.FirstOrDefault(m => m.OperaID == id);
+
+            if (o == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(o);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(opera).State = EntityState.Deleted;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(opera);
+
+        }*/
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Opera o = context.Operas.FirstOrDefault(m => m.OperaID == id);
+
+            if (o == null)
+            {
+                return HttpNotFound();
+            }
+
+            var opera=context.Operas.Find(id);
+
+            context.Operas.Remove(o);
+            context.SaveChanges();
+
+
+            return View("Index");
+        }
+
+    }
+}

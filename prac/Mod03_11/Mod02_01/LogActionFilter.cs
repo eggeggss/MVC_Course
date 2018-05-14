273be99b5415cd073efb211f97c69c2ba6e7152d
eggeggss <eggeggss@gmail.com>
2018-05-14 21:54:19 +0800

    v1

---
+++ prac/Mod03_11/Mod02_01/LogActionFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod02_01
+{
+    public class LogActionFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            System.Diagnostics.Debug.WriteLine("OnActionExecuted");
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            System.Diagnostics.Debug.WriteLine("OnActionExecuting");
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            System.Diagnostics.Debug.WriteLine("OnResultExecuted");
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            System.Diagnostics.Debug.WriteLine("OnResultExecuting");
+        }
+    }
+}
---
+++ prac/Mod03_12/Mod02_01/LogActionFilter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Mod02_01
+{
+
+    
+    public class LogActionFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Log("OnActionExecuted", filterContext.RouteData);
+
+
+            //System.Diagnostics.Debug.WriteLine("OnActionExecuted");
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Log("OnActionExecuting", filterContext.RouteData);
+
+            // System.Diagnostics.Debug.WriteLine("OnActionExecuting");
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Log("OnResultExecuted", filterContext.RouteData);
+
+            //System.Diagnostics.Debug.WriteLine("OnResultExecuted");
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Log("OnResultExecuting", filterContext.RouteData);
+
+            //System.Diagnostics.Debug.WriteLine("OnResultExecuting");
+        }
+
+        private void Log(string mehtodName,RouteData routeDate)
+        {
+            var controllerName = routeDate.Values["controller"];
+            var actionName = routeDate.Values["action"];
+            var message = $"methodname:{mehtodName} controller:{controllerName},action:{actionName}";
+        }
+
+    }
+}

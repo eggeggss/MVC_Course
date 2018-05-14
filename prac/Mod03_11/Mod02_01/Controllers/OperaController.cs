c042e464fc8e462bfb205f99892d10e9165c9524
eggeggss <eggeggss@gmail.com>
2018-05-14 19:24:20 +0800

    v1

---
+++ prac/Mod03_11/Mod02_01/Controllers/OperaController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mod02_01.DAL;
+using Mod02_01.Models;
+
+namespace Mod02_01.Controllers
+{
+    public class OperaController : Controller
+    {
+        private OperaContext db = new OperaContext();
+
+        // GET: Opera
+        public async Task<ActionResult> Index()
+        {
+
+            System.Diagnostics.Debug.WriteLine("Index", "DEBUG");
+           
+            return View(await db.Operas.ToListAsync());
+        }
+
+        // GET: Opera/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //await Task.Delay(5000);
+            Opera opera = await db.Operas.FindAsync(id);
+            if (opera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(opera);
+        }
+
+        // GET: Opera/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Opera/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "OperaID,Title,Year,Composer")] Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Operas.Add(opera);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(opera);
+        }
+
+        // GET: Opera/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Opera opera = await db.Operas.FindAsync(id);
+            if (opera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(opera);
+        }
+
+        // POST: Opera/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "OperaID,Title,Year,Composer")] Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(opera).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(opera);
+        }
+
+        // GET: Opera/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Opera opera = await db.Operas.FindAsync(id);
+            if (opera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(opera);
+        }
+
+        // POST: Opera/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Opera opera = await db.Operas.FindAsync(id);
+            db.Operas.Remove(opera);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

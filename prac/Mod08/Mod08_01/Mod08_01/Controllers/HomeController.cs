99f7129b0f57a861a154edf0e23cf45a35cba17d
eggeggss <eggeggss@gmail.com>
2018-05-23 20:45:39 +0800

    v1

---
+++ prac/Mod08/Mod08_01/Mod08_01/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod08_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Call()
+        {
+            ViewBag.Title = "台大遴委遭約談 北檢：依法辦理 盼勿混淆焦點";
+            return PartialView("_Call");
+        }
+
+
+        public ActionResult Call2(int? id)
+        {
+            ViewBag.Title =id+ "--台大遴委遭約談 北檢：依法辦理 盼勿混淆焦點";
+            return PartialView("_Call");
+        }
+    }
+}
---
+++ prac/Mod08/Mod08_01/Mod08_01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mod08_01.MvcApplication" Language="C#" %>

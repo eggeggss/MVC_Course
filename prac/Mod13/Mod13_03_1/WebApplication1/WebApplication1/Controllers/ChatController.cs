bc575f40962db134af9f81e1933948b4da003443
eggeggss <eggeggss@gmail.com>
2018-06-04 21:35:34 +0800

    v1

---
+++ prac/Mod13/Mod13_03_1/WebApplication1/WebApplication1/Controllers/ChatController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ChatController : ApiController
+    {
+        // GET: Chat
+        public HttpResponseMessage Get(string username)
+        {
+            HttpContext.Current.AcceptWebSocketRequest(new ChatWebSocketHandler(username));
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.SwitchingProtocols);
+
+        }
+    }
+
+    public class ChatWebSocketHandler:WebSocketHandler
+    {
+        private static WebSocketCollection _chatClients = new WebSocketCollection();
+        private string _username;
+        public ChatWebSocketHandler(string username)
+        {
+            _username = username;
+        }
+
+        public override void OnOpen()
+        {
+            _chatClients.Add(this);
+        }
+        public override void OnMessage(string message)
+        {
+            _chatClients.Broadcast(_username + ":" + message);
+           
+        }
+
+    }
+}
---
+++ prac/Mod14/Mod14_01/Mod14_01/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mod14_01.MvcApplication" Language="C#" %>
